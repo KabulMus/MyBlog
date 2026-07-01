b8a62a202d734696087c743f3e05070de4c771b9
EthanShaw <szs_123456@outlook.com>
2026-07-01 19:49:35 +0800

    Fixed the nav blur bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,14 @@ export default defineConfig({
     remarkPlugins: [remarkMath], // ⚡️ 负责在 Markdown 编译阶段识别 $ 和 $$ 语法
     rehypePlugins: [rehypeKatex], // ⚡️ 负责把识别出的公式转换成高精度的 HTML/CSS 语义标签
   },
+  vite: {
+    css: {
+      // ⚡️ 方案 2-1：直接强制指定 vite 的 CSS 目标，覆盖默认的激进压缩
+      target: ['chrome80', 'safari13', 'firefox75', 'edge80']
+    },
+    build: {
+      // ⚡️ 方案 2-2：如果上面还不行，可以尝试把默认的 cssMinify 换成 esbuild（更守规矩）
+      cssMinify: 'esbuild' 
+    }
+  }
 });
